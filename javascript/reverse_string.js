5e84b5c86bb84f2a753c2c43dd7cb93a24402224
troymeeker <troymeeker1@gmail.com>
2022-07-21 12:06:03 -0700

    solved

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
-  // type your code here
+  // type your code here 
+   let rev = ''
+   for(let i of str){
+    rev = i + rev
+   }
+   return rev
+
 }
 
 if (require.main === module) {
@@ -17,3 +23,7 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+// for loop,
+// set result to empty string, for loop, 
